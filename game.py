d516f6bf9e3396a8caa232d2000b88451d547e4d
CMP_DaLysychenko <danylollysychenko@gmail.com>
2025-09-24 08:55:01 -0400

    Finished phase 3 of rock paper scissors game

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import random
-
+player_score = 0
+ai_score = 0
 def convert_letter_to_word(letter):
+    """Converts letter to word and return the full word"""
+
     if letter == "r":
         return "rock"
     elif letter == "s":
@@ -9,53 +12,83 @@ def convert_letter_to_word(letter):
         return "paper"
     
 def print_series_results(user_wins, comp_wins):
+    """Prints who won the full series of 5 rounds"""
+
     print(f"AI won {comp_wins} rounds.")
     print(f"You won {user_wins} rounds.\n")
 
     if comp_wins > user_wins:
-        print(f"AI won.")
+        print(f"AI won the series.")
     elif comp_wins < user_wins:
-        print("You won.")
+        print("You won the series.")
     else:
-        print("It's a tie.")
+        print("It's a tie.\nNobody won.")
 
 def determine_outcome(user, comp):
-    pass
+    """ determines the outcome and returns the result of whatever condition(s) it falls under """
+
+    if comp == "rock" and user == "rock" or comp == "scissors" and user == "scissors" or comp == "paper" and user == "paper":
+        return "tie"
+    elif comp == "rock" and user == "scissors" or comp == "scissors" and user == "paper" or comp == "paper" and user == "rock":
+        return "AI won"
+    elif comp == "rock" and user == "paper" or comp == "scissors" and user == "rock" or comp == "paper" and user == "scissors":
+        return "you won"
 
 def print_round_results(user, comp, result):
-    pass
+    """Makes the variables global so the whole program can use it and prints the round result after checking conditions from determine outcome"""
 
-def get_valid_input():
-    pass
+    global player_score , ai_score
 
-comp_moves = ["r", "s", "p"]
-player_score = 0
-ai_score = 0
+    if result == "tie":
+        print(f"You played \" {user} \" the computer played \" {comp} \" it's a tie.\n")
 
-for i in range(5):
-    while True:
-        print(f"Round {i+1}")
-        player_move = input("Enter r, p, s: ").strip().lower()
-        print()
-        if player_move not in comp_moves:
-            print("Invalid input.")
-            player_move = input("Enter r, p, s: ").strip().lower()
-        else:
-            break
-
-    ai_choice = random.choice(comp_moves)
-    ai_choice = convert_letter_to_word(ai_choice)
-    player_move = convert_letter_to_word(player_move)
-    
-    if ai_choice == "rock" and player_move == "rock" or ai_choice == "scissors" and player_move == "scissors" or ai_choice == "paper" and player_move == "paper":
-        print(f"You played \" {player_move} \" the computer played \" {ai_choice} \" it's a tie.\n")
-
-    elif ai_choice == "rock" and player_move == "scissors" or ai_choice == "scissors" and player_move == "paper" or ai_choice == "paper" and player_move == "rock":
-        print(f"You played \" {player_move} \" the computer played \" {ai_choice} \" ai won.\n")
+    elif result == "AI won":
+        print(f"You played \" {user} \" the computer played \" {comp} \" ai won.\n")
         ai_score += 1
 
-    elif ai_choice == "rock" and player_move == "paper" or ai_choice == "scissors" and player_move == "rock" or ai_choice == "paper" and player_move == "scissors":
-        print(f"You played \" {player_move} \" the computer played \" {ai_choice} \" you won.\n")
+    elif result == "you won":
+        print(f"You played \" {user} \" the computer played \" {comp} \" you won.\n")
         player_score += 1
 
-print_series_results(player_score, ai_score)
+def get_valid_input():
+    """Get the valid from the user and returns player_move"""
+    """
+    while True:
+    -get move
+    if move is valid in comp_moves
+    return move
+    print invalid move
+
+    """
+    
+    while True:
+        player_move = input(f"Enter r, p, s: ").strip().lower()
+        print()
+
+        if player_move in comp_moves:
+            return player_move
+        
+        print("Invalid input.")
+
+global comp_moves
+comp_moves = ["r", "s", "p"]
+
+def main():
+    for i in range(5):
+        print(f"Round {i+1}")
+
+        player_move = get_valid_input()
+
+        ai_choice = random.choice(comp_moves)
+        ai_choice = convert_letter_to_word(ai_choice)
+        player_move = convert_letter_to_word(player_move)
+        
+        
+            
+        result = determine_outcome(player_move, ai_choice)
+        print_round_results(player_move, ai_choice, result)
+
+    print_series_results(player_score, ai_score)
+
+if __name__ == "__main__":
+    main()
